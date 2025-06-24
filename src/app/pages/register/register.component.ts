14ab3759bdefc5757e6d2312d46c8981d3533af0
AndersonFerrer <ferrer.280104@gmail.com>
2025-06-24 16:41:59 -0500

    feat(auth): Implementacion de formularios reactivos para los componentes de registro y login.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-register',
-    imports: [],
-    templateUrl: './register.component.html',
-    styleUrl: './register.component.css'
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+  imports: [ReactiveFormsModule],
 })
 export class RegisterComponent {
-    imagenUrl: string = 'assets/XperienceCampusLogo.png';
+  registerForm: FormGroup;
+  imagenUrl: string = 'assets/XperienceCampusLogo.png';
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group(
+      {
+        nombres: ['', Validators.required],
+        apellidos: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password')?.value === g.get('confirmPassword')?.value
+      ? null
+      : { mismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      // Aqui enviariamos los datos a la api
+      console.log(this.registerForm.value);
+    }
+  }
 }
